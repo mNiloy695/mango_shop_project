09ce688c48f9e0544cadcada82356b5b99f3d7c0
mNiloy695 <mniloy695@gmail.com>
2024-07-28 20:37:58 +0600

    updated

--- mango_shop/settings.py
+++ mango_shop/settings.py
@@ -33,7 +33,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS= True
 CSRF_TRUSTED_ORIGINS = ['https://mango-shop-project-2.onrender.com']
 
 # Application definition
