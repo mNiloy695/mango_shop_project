ba773224ee89829f55ee8b982e5553a8f81231eb
mNiloy695 <mniloy695@gmail.com>
2024-08-02 18:37:27 +0600

    purchase_model_updated

--- purchase/serializers.py
+++ purchase/serializers.py
@@ -14,29 +14,29 @@ class PurchaseModelSerializer(serializers.ModelSerializer):
         model=models.PurchaseModel
         fields='__all__'
     
-    # def save(self):
-    #     mango=self.validated_data["mango"]
-    #     user=self.validated_data["user"]
-    #     order_status=self.validated_data["order_status"]
-    #     quantity=self.validated_data["quantity"]
-    #     price=mango.price * quantity
-    #     address=self.validated_data['address']
+    def save(self):
+        mango=self.validated_data["mango"]
+        user=self.validated_data["user"]
+        order_status=self.validated_data["order_status"]
+        quantity=self.validated_data["quantity"]
+        price=mango.price * quantity
+        address=self.validated_data['address']
         
-    #     if quantity>mango.weight:
-    #         raise serializers.ValidationError({'error':"Our Stock is low"})
-    #     if quantity<2:
-    #         raise serializers.ValidationError({'error':"You need to buy at least 2 kg"})
+        if quantity>mango.weight:
+            raise serializers.ValidationError({'error':"Our Stock is low"})
+        if quantity<2:
+            raise serializers.ValidationError({'error':"You need to buy at least 2 kg"})
         
-    #     purchase=models.PurchaseModel(mango=mango,user=user,order_status=order_status,quantity=quantity,price=price,address=address)
-    #     man=models.MangoModel.objects.get(id=mango.id)
-    #     purchase.save()
+        purchase=models.PurchaseModel.objects.get_or_create(mango=mango,user=user,order_status=order_status,quantity=quantity,price=price,address=address)
+        man=models.MangoModel.objects.get(id=mango.id)
+        purchase.save()
 
-    #     if  purchase.order_status=="pending":
-    #         man.weight-=purchase.quantity
-    #         man.save()
-    #     elif purchase.order_status =="cancelled":
-    #         man.weight+=purchase.quantity
-    #         man.save()
+        if  purchase.order_status=="pending":
+            man.weight-=purchase.quantity
+            man.save()
+        elif purchase.order_status =="cancelled":
+            man.weight+=purchase.quantity
+            man.save()
 
 
 # review  serializer
