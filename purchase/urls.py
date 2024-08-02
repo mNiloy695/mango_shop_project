e8762d255db5656114e0da8b3573f826b0942ad9
mNiloy695 <mniloy695@gmail.com>
2024-08-03 01:34:55 +0600

    update_view

--- purchase/urls.py
+++ purchase/urls.py
@@ -5,13 +5,14 @@ from . import views
 
 
 router=DefaultRouter()
-router.register('purchase',views.PurchaseSerializerViewSet)
+# router.register('purchase',views.PurchaseSerializerViewSet)
 router.register('review',views.ReviewSerializerViewSet)
 router.register('user/addresses',views.AddressViewSet)
 
 urlpatterns = [
    
     path('',include(router.urls)),
+    path('purchase/',views.PurchaseSerializerViewSet.as_view(),name="purchase"),
 ]
 
 # urlpatterns = [
--- purchase/views.py
+++ purchase/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from . import models
 from rest_framework import viewsets
 from rest_framework import filters
-
+from rest_framework import status
 class PurchaseModelFilterForSpecificUser(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         user_id=request.query_params.get('user_id')
@@ -15,13 +15,19 @@ class PurchaseModelFilterForSpecificUser(filters.BaseFilterBackend):
             return queryset.filter(user=user_id)
         return queryset
 
-class PurchaseSerializerViewSet(viewsets.ModelViewSet):
-    permission_classes=[permissions.IsAuthenticated]
-    queryset=models.PurchaseModel.objects.all()
-    serializer_class=serializers.PurchaseModelSerializer
-    filter_backends=[PurchaseModelFilterForSpecificUser,filters.SearchFilter]
-    search_fields=['PurchaseModel__id']
-
+class PurchaseSerializerViewSet(APIView):
+   def get(self,request,format=None):
+       print("get method")
+       purchases=models.PurchaseModel.objects.all()
+       serializer=serializers.PurchaseModelSerializer(purchases,many=True)
+       return Response(serializer.data)
+   def post(self,request,format=None):
+       purchase=request.data
+       serializer=serializers.PurchaseModelSerializer(data=purchase)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data,status=status.HTTP_201_CREATED)
+       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 class ReviewFilterForSpecificMango(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         mango_id=request.query_params.get('mango_id')
